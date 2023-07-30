1f88e34e33e42bb8efef0c0fd089ddb36deab160
Noreh AD <akinoreh@gmail.com>
2023-07-30 17:10:22 +0300

    custom-scrollbar

--- custom-scrollbar/controls.js
+++ custom-scrollbar/controls.js
@@ -25,31 +25,37 @@ let controls = {
 };
 
 window.addEventListener("AllScriptsLoaded", () => {
-    controls.scrollbar.width = qs("#scrollbar-width-input");
+    controls.scrollbar.width  = qs("#scrollbar-width-input");
     controls.scrollbar.height = qs("#scrollbar-height-input");
     
-    on([
-        controls.scrollbar.width,
-        qs("#scrollbar-width-range"),
-    ], "input", function () {
+    controls["scrollbar-track"]["background-color"] = qs("#track-background-input");
+    controls["scrollbar-track"]["border-radius"]    = qs("#track-radius-input");
+    
+    on([controls.scrollbar.width, qs("#scrollbar-width-range")], "input", function () {
         if (!["0", "", NaN].includes(this.value)) {
             css.scrollbar.width = this.value + "px";
         }
     });
-    on([
-        controls.scrollbar.height,
-        qs("#scrollbar-height-range"),
-    ], "input", function () {
+    on([controls.scrollbar.height, qs("#scrollbar-height-range")], "input", function () {
         if (!["0", "", NaN].includes(this.value)) {
             css.scrollbar.height = this.value + "px";
         }
     });
     
+    on([controls["scrollbar-track"]["background-color"]], "input", function () {
+        css["scrollbar-track"]["background-color"] = this.value;
+    });
+    on([controls["scrollbar-track"]["border-radius"], qs("#track-radius-range")], "input", function () {
+        if (!["0", "", NaN].includes(this.value)) {
+            css["scrollbar-track"]["border-radius"] = this.value + "px";
+        }
+    });
+    
     on([
-        controls.scrollbar.width,
-        qs("#scrollbar-width-range"),
-        controls.scrollbar.height,
-        qs("#scrollbar-height-range"),
+        controls.scrollbar.width,  qs("#scrollbar-width-range"),
+        controls.scrollbar.height, qs("#scrollbar-height-range"),
+        controls["scrollbar-track"]["background-color"],
+        controls["scrollbar-track"]["border-radius"], qs("#track-radius-range"),
     ], "input", function () {
         applyCSS();
         outputCSS();
@@ -57,7 +63,18 @@ window.addEventListener("AllScriptsLoaded", () => {
     
     syncNumberInputAndRange(controls.scrollbar.width, qs("#scrollbar-width-range"));
     syncNumberInputAndRange(controls.scrollbar.height, qs("#scrollbar-height-range"));
+    syncColorAndTextInput(controls["scrollbar-track"]["background-color"], qs("#track-background-text"));
+    syncNumberInputAndRange(controls["scrollbar-track"]["border-radius"], qs("#track-radius-range"));
 });
+
+function syncColorAndTextInput(color, textInput) {
+    color.addEventListener("input", () => {
+        textInput.value = color.value;
+    });
+    textInput.addEventListener("input", () => {
+        color.value = textInput.value;
+    });
+}
 
 function syncNumberInputAndRange(numberInput, range) {
     numberInput.addEventListener("input", () => {
