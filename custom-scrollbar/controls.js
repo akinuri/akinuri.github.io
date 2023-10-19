70fefb43f3008b65439b386343ca306510679840
Noreh AD <akinoreh@gmail.com>
2023-10-19 18:49:18 +0300

    custom-scrollbar

--- custom-scrollbar/controls.js
+++ custom-scrollbar/controls.js
@@ -1,6 +1,7 @@
 let controls = {
     "preview" : {
         "background-color" : null,
+        "text-color" : null,
     },
     "scrollbar" : {
         "width" : {
@@ -79,6 +80,11 @@ sl.onAllLoaded(() => {
     controls.preview["background-color"] = qs("#preview-background-input");
     on(controls.preview["background-color"], "input", function () {
         qs("#preview-card").style.setProperty("--preview-bg", this.value);
+    });
+    
+    controls.preview["text-color"] = qs("#preview-text-color-input");
+    on(controls.preview["text-color"], "input", function () {
+        qs("#preview-card").style.setProperty("--preview-text-color", this.value);
     });
     
     
@@ -326,43 +332,6 @@ sl.onAllLoaded(() => {
     });
     
 });
-
-function getBackgroundAlphaHex(input) {
-    let alpha = parseFloat(input.value) || 0;
-    let alphaScaled = Math.round(alpha * 255);
-    let alphaHex = alphaScaled.toString(16);
-    return alphaHex;
-}
-
-function getBackground(colorInput, alphaInput) {
-    let colorHex = colorInput.value;
-    let alphaHex = getBackgroundAlphaHex(alphaInput);
-    if (alphaHex == 0) {
-        alphaHex = "00";
-    }
-    if (alphaHex != "ff") {
-        colorHex += alphaHex;
-    }
-    return colorHex;
-}
-
-function syncColorAndTextInput(color, textInput) {
-    color.addEventListener("input", () => {
-        textInput.value = color.value;
-    });
-    textInput.addEventListener("input", () => {
-        color.value = textInput.value;
-    });
-}
-
-function syncNumberInputAndRange(numberInput, range) {
-    numberInput.addEventListener("input", () => {
-        range.value = numberInput.value || 0;
-    });
-    range.addEventListener("input", () => {
-        numberInput.value = range.value;
-    });
-}
 
 function outputCSS(selectorPrefix = ".scrollbar") {
     let cssOutput = qs("#css-output");
