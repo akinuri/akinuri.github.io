89a32170618db6c7acbb1ca3c23db4faecad9a7b
Noreh AD <akinoreh@gmail.com>
2024-06-13 16:16:49 +0300

    swipe-velocity

--- swipe-velocity/app.js
+++ swipe-velocity/app.js
@@ -1,16 +1,10 @@
-let shape = {
-    el : qs("#shape"),
-    width: qs("#shape").clientWidth,
-    height: qs("#shape").clientHeight,
-};
+let shape = new Circle(qs("#shape"));
 
-let bounds = {
-    width : innerWidth,
-    height : innerHeight,
-};
+let container = new Rectangle(innerWidth, innerHeight);
+
 on(window, "resize", () => {
-    bounds.width = innerWidth;
-    bounds.height = innerHeight;
+    container.width = innerWidth;
+    container.height = innerHeight;
 });
 
 function onDragStart(event) {
@@ -18,17 +12,39 @@ function onDragStart(event) {
     const isTouchEvent = event.type === "touchstart";
     const startX = isTouchEvent ? event.touches[0].clientX : event.clientX;
     const startY = isTouchEvent ? event.touches[0].clientY : event.clientY;
-    const offsetX = startX - shape.el.getBoundingClientRect().left;
-    const offsetY = startY - shape.el.getBoundingClientRect().top;
+    let lastX = startX;
+    let lastY = startY;
+    let lastTime = performance.now();
+    const offsetX = event.offsetX;
+    const offsetY = event.offsetY;
     function onMove(event) {
         const moveX = isTouchEvent ? event.touches[0].clientX : event.clientX;
         const moveY = isTouchEvent ? event.touches[0].clientY : event.clientY;
         let x = moveX - offsetX;
         let y = moveY - offsetY;
-        x = clamp(x, 0, bounds.width - shape.width);
-        y = clamp(y, 0, bounds.height - shape.height);
+        x = clamp(x, 0, container.width - shape.radius);
+        y = clamp(y, 0, container.height - shape.radius);
         shape.el.style.left = `${x}px`;
         shape.el.style.top = `${y}px`;
+        stats.pos.x.textContent = x + shape.radius;
+        stats.pos.y.textContent = y + shape.radius;
+
+        const currentTime = performance.now();
+        const deltaTime = (currentTime - lastTime) / 1000;
+        const deltaX = moveX - lastX;
+        const deltaY = moveY - lastY;
+        
+        const velX = deltaX / deltaTime;
+        const velY = deltaY / deltaTime;
+        
+        let angle = calcRelAngleDegrees(-lastX, lastY, -moveX, moveY);
+        stats.vel.mag.textContent = getMagnitude(velX, velY).toFixed(1);
+        stats.vel.ang.textContent = angle.toFixed(1) + "°";
+        stats.vel.angIcon.style.setProperty("--rotate", angle + "deg");
+        
+        lastX = moveX;
+        lastY = moveY;
+        lastTime = currentTime;
     }
     function onDragEnd() {
         off(document, isTouchEvent ? "touchmove" : "mousemove", onMove);
---
+++ swipe-velocity/stats.js
@@ -0,0 +1,11 @@
+let stats = {
+    pos : {
+        x : qs("#posX"),
+        y : qs("#posY"),
+    },
+    vel : {
+        mag : qs("#velMag"),
+        ang : qs("#velAng"),
+        angIcon : qs("svg"),
+    },
+};
