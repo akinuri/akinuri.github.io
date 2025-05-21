0a9c2109366e07f0c3be1bbfdd0b3d7e59fd4605
Noreh AD <akinoreh@gmail.com>
2025-05-21 22:18:53 +0300

    24h-clock

--- 24h-clock/clock-utils.js
+++ 24h-clock/clock-utils.js
@@ -2,69 +2,142 @@ function drawLinearClock(context) {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     context.save();
 
-    for (let hour = 0; hour < 24; hour++) {
-        let x = hour * oneHourWidthPx;
-        x += offsetPx;
-        x = loop(x, 0, context.canvas.width, "close");
+    hourMarks: {
+        for (let hour = 0; hour < 24; hour++) {
+            let x = hour * oneHourWidthPx;
+            x += offsetPx;
+            x = loop(x, 0, context.canvas.width, "close");
 
-        const gradient = context.createLinearGradient(0, 0, 0, context.canvas.height / 2);
-        gradient.addColorStop(0, "hsla(0, 0%, 50%, 0.5)");
-        gradient.addColorStop(1, "hsla(0, 0%, 50%, 0)");
+            const gradient = context.createLinearGradient(0, 0, 0, context.canvas.height / 2);
+            gradient.addColorStop(0, "hsl(0, 0%, 50%, 0.5)");
+            gradient.addColorStop(1, "hsl(0, 0%, 50%, 0)");
 
-        context.beginPath();
-        context.moveTo(x + 0.5, 0);
-        context.lineTo(x + 0.5, context.canvas.height);
-        context.strokeStyle = gradient;
-        if (hour == 0) {
-            context.strokeStyle = "hsl(0, 0%, 0%, 0.33)";
+            context.beginPath();
+            context.moveTo(x + 0.5, 0);
+            context.lineTo(x + 0.5, context.canvas.height);
+            context.strokeStyle = gradient;
+            if (hour == 0) {
+                context.strokeStyle = "hsl(0, 0%, 0%, 0.33)";
+            }
+            context.lineWidth = 1;
+            context.stroke();
+
+            context.font = "14px Arial";
+            context.fontWeight = "bold";
+            context.fillStyle = "hsl(0, 0%, 0%, 0.5)";
+            if (hour == 0) {
+                context.fillStyle = "hsl(0, 0%, 0%)";
+            }
+            context.fillText(hour, x + 5, 20);
         }
-        context.lineWidth = 1;
-        context.stroke();
-
-        context.font = "14px Arial";
-        context.fontWeight = "bold";
-        context.fillStyle = "dimgray";
-        if (hour == 0) {
-            context.fillStyle = "black";
-        }
-        context.fillText(hour, x + 5, 20);
     }
 
-    for (let minute = 0; minute < 60; minute++) {
-        let x = minute * oneMinuteWidthPx;
-        x += offsetPx;
-        x = loop(x, 0, context.canvas.width, "close");
+    minuteMarks: {
+        for (let minute = 0; minute < 60; minute++) {
+            let x = minute * oneMinuteWidthPx;
+            x += offsetPx;
+            x = loop(x, 0, context.canvas.width, "close");
 
-        const gradient = context.createLinearGradient(0, context.canvas.height / 2, 0, context.canvas.height);
-        gradient.addColorStop(0, "hsla(0, 0%, 50%, 0)");
-        gradient.addColorStop(1, "hsla(0, 0%, 50%, 0.5)");
+            const gradient = context.createLinearGradient(0, context.canvas.height * 0.9, 0, context.canvas.height);
+            gradient.addColorStop(0, "hsl(0, 0%, 50%, 0)");
+            gradient.addColorStop(1, "hsl(0, 0%, 50%, 0.5)");
+
+            context.save();
+            context.beginPath();
+            context.moveTo(x + 0.5, context.canvas.height * 0.9);
+            context.lineTo(x + 0.5, context.canvas.height);
+            context.strokeStyle = gradient;
+            context.lineWidth = 1;
+            context.stroke();
+            context.restore();
+
+            if (minute % 5 == 0) {
+                const gradient = context.createLinearGradient(
+                    0,
+                    context.canvas.height * 0.66,
+                    0,
+                    context.canvas.height
+                );
+                gradient.addColorStop(0, "hsl(0, 0%, 50%, 0)");
+                gradient.addColorStop(1, "hsl(0, 0%, 50%, 0.5)");
+                context.save();
+                context.beginPath();
+                context.moveTo(x + 0.5, context.canvas.height * 0.66);
+                context.lineTo(x + 0.5, context.canvas.height);
+                context.strokeStyle = gradient;
+                context.lineWidth = 1;
+                context.stroke();
+                context.restore();
+
+                context.font = "12px Arial";
+                context.fontWeight = "bold";
+                context.fillStyle = "hsl(0, 0%, 0%, 0.4)";
+                context.fillText(minute, x + 5, context.canvas.height * 0.85);
+            }
+        }
+    }
+
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    hourHand: {
+        let hourX = (context.canvas.width / 24) * (hour + (minute / 60) + (second / 3600));
+        hourX += offsetPx;
+        hourX = loop(hourX, 0, context.canvas.width, "close");
 
         context.save();
+        const gradient = context.createLinearGradient(0, 0, 0, context.canvas.height * 0.66);
+        gradient.addColorStop(0, "hsl(240, 100%, 50%, 0.5)");
+        gradient.addColorStop(1, "hsl(240, 100%, 50%, 0)");
+
         context.beginPath();
-        context.moveTo(x + 0.5, context.canvas.height / 2);
-        context.lineTo(x + 0.5, context.canvas.height);
+        context.moveTo(hourX + 0.5, 0);
+        context.lineTo(hourX + 0.5, context.canvas.height * 0.66);
         context.strokeStyle = gradient;
-        context.lineWidth = 1;
+        context.lineWidth = 4;
         context.stroke();
         context.restore();
     }
 
-    let date = new Date();
-    let second = date.getSeconds();
-    let secondX = (context.canvas.width / 60) * second;
-    secondX += offsetPx;
-    secondX = loop(secondX, 0, context.canvas.width, "close");
+    minuteHand: {
+        let minuteX = (context.canvas.width / 60) * (minute + (second / 60));
+        minuteX += offsetPx;
+        minuteX = loop(minuteX, 0, context.canvas.width, "close");
 
-    const gradient = context.createLinearGradient(0, context.canvas.height / 3, 0, context.canvas.height);
-    gradient.addColorStop(0, "hsla(0, 100%, 50%, 0)");
-    gradient.addColorStop(1, "hsla(0, 100%, 50%, 0.5)");
+        context.save();
+        const gradient = context.createLinearGradient(0, context.canvas.height * 0.5, 0, context.canvas.height);
+        gradient.addColorStop(0, "hsl(120, 90%, 40%, 0)");
+        gradient.addColorStop(1, "hsl(120, 90%, 40%, 0.75)");
 
-    context.beginPath();
-    context.moveTo(secondX + 0.5, 0);
-    context.lineTo(secondX + 0.5, context.canvas.height);
-    context.strokeStyle = gradient;
-    context.lineWidth = 2;
-    context.stroke();
+        context.beginPath();
+        context.moveTo(minuteX + 0.5, context.canvas.height * 0.5);
+        context.lineTo(minuteX + 0.5, context.canvas.height);
+        context.strokeStyle = gradient;
+        context.lineWidth = 3;
+        context.stroke();
+        context.restore();
+    }
+
+    secondHand: {
+        let secondX = (context.canvas.width / 60) * second;
+        secondX += offsetPx;
+        secondX = loop(secondX, 0, context.canvas.width, "close");
+
+        context.save();
+        const gradient = context.createLinearGradient(0, context.canvas.height * 0.33, 0, context.canvas.height);
+        gradient.addColorStop(0, "hsl(0, 100%, 50%, 0)");
+        gradient.addColorStop(1, "hsl(0, 100%, 50%, 0.5)");
+
+        context.beginPath();
+        context.moveTo(secondX + 0.5, context.canvas.height * 0.33);
+        context.lineTo(secondX + 0.5, context.canvas.height);
+        context.strokeStyle = gradient;
+        context.lineWidth = 1.5;
+        context.stroke();
+        context.restore();
+    }
 
     context.restore();
 }
